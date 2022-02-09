8da07f66878f1443ec101f54ba2183d3bb3d8fd1
Sami Salonen <ssalonen@gmail.com>
2022-02-09 20:12:31 +0200

    volume indication WIP
    Signed-off-by: Sami Salonen <ssalonen@gmail.com>

--- src/bin.rs
+++ src/bin.rs
@@ -2,10 +2,11 @@ use log::{debug, error, info, trace, warn};
 
 extern crate cec_rs;
 use cec_rs::{
-    CecCommand, CecConnectionCfgBuilder, CecDeviceType, CecDeviceTypeVec, CecKeypress,
-    CecLogMessage, CecUserControlCode,
+    CecCommand, CecConnection, CecConnectionCfgBuilder, CecDeviceType, CecDeviceTypeVec,
+    CecKeypress, CecLogMessage, CecUserControlCode,
 };
 use std::process::Command;
+use std::sync::mpsc::{channel, Sender};
 
 use std::{thread, time};
 
@@ -31,12 +32,21 @@ fn on_key_press(keypress: CecKeypress) {
     }
 }
 
-fn on_command_received(command: CecCommand) {
+fn on_command_received(sender: Sender<CecCommand>, command: CecCommand) {
     trace!(
         "onCommandReceived:  opcode: {:?}, initiator: {:?}",
         command.opcode,
         command.initiator
     );
+    match command.opcode {
+        cec_rs::CecOpcode::GiveSystemAudioModeStatus => {
+            // report audio mode
+        }
+        cec_rs::CecOpcode::GiveAudioStatus => {
+            // report volume
+        }
+        _ => {}
+    };
 }
 
 fn on_log_message(log_message: CecLogMessage) {
@@ -53,19 +63,33 @@ fn on_log_message(log_message: CecLogMessage) {
 
 pub fn main() {
     env_logger::init();
+    let (sender, receiver) = channel();
 
     let cfg = CecConnectionCfgBuilder::default()
         .port("RPI".into())
         .device_name("Hifiberry".into())
         .key_press_callback(Box::new(on_key_press))
-        .command_received_callback(Box::new(on_command_received))
+        .command_received_callback(Box::new(move |command| {
+            on_command_received(sender, command) // TODO: Command to implement copy?
+        }))
         .log_message_callback(Box::new(on_log_message))
         .device_types(CecDeviceTypeVec::new(CecDeviceType::AudioSystem))
         .build()
         .unwrap();
-    let connection = cfg.open().unwrap();
+    let connection: CecConnection = cfg.open().unwrap();
+
     trace!("Active source: {:?}", connection.get_active_source());
 
-    thread::sleep(time::Duration::from_secs(99_999_999));
+    loop {
+        if let Ok(command) = receiver.recv() {
+            if let Err(e) = connection.transmit(command.into()) {
+                // FIXME: fix cec-rs signature
+                // warn!("Could not send command {}: {}", command.opcode, e) // FIXME:display
+            }
+        } else {
+            break;
+        }
+    }
+
     // TODO: handle alsa vol changes
 }
